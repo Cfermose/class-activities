4bf4c26ec5fafa4914fa33d843d52fd6a3920740
Cfermose <carolinefermoselle@gmail.com>
2021-03-13 14:01:23 -0500

    finished

--- 19.1activities/01_Hello_World_Again/unsolved/js/index.js
+++ 19.1activities/01_Hello_World_Again/unsolved/js/index.js
@@ -1,3 +1,24 @@
 // Create a variable below
 
+
+var helloWorld = 'Hello this is Caroline'
+
 // Console.log() your variable here.
+
+
+
+console.log(helloWorld);
+
+
+var x = '3';
+var y = '2';
+var z= 'there are ' + x + y + ' people outside';
+
+
+/*this defines the function */
+function addkids() {
+  alert (z);
+}
+
+/*this makes the function work*/
+addkids();
